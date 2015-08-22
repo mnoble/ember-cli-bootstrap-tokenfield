650353db92c64573837b8305ce95d8f88a6178f2
Matte Noble <me@mattenoble.com>
2015-08-22 09:52:53 -0700

    Remove bootstrap CSS

--- index.js
+++ index.js
@@ -4,17 +4,7 @@
 module.exports = {
   included: function(app) {
     this._super.included(app);
-
-    // Bootstrap
-    app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-
-    // JQuery UI
     app.import('bower_components/jquery-ui/jquery-ui.js');
-    app.import('bower_components/jquery-ui/themes/base/jquery-ui.css');
-
-    // Bootstrap Tokenfield 
-    app.import('bower_components/bootstrap-tokenfield/dist/css/tokenfield-typeahead.css');
-    app.import('bower_components/bootstrap-tokenfield/dist/css/bootstrap-tokenfield.css');
     app.import('bower_components/bootstrap-tokenfield/dist/bootstrap-tokenfield.js');
   },
 
